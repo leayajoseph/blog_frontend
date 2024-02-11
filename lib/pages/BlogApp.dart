6359e045e5668e84e953987033f684fdd486fd52
leayajoseph <leaya231@gmail.com>
2024-02-12 01:40:24 +0530

    main page created

--- lib/pages/BlogApp.dart
+++ lib/pages/BlogApp.dart
@@ -5,6 +5,46 @@ class BlogApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Blog App",
+          style: TextStyle(color: Colors.white),),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          width: double.infinity,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Colors.blue.shade900.withOpacity(0.4),
+                    Colors.blue.shade500.withOpacity(0.4),
+                    Colors.blue.shade300.withOpacity(0.4),
+                  ]
+              )
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              ElevatedButton(onPressed: ()
+                  {
+
+                  }, child: Text("Add Blog")),
+              SizedBox(height: 10,),
+              ElevatedButton(onPressed: ()
+              {
+
+              }, child: Text("View Blog")),
+              SizedBox(height: 10,),
+
+
+            ],
+
+          ),
+        ),
+      )
+    );
   }
 }
